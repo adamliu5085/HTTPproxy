a0d273c49acdb665669b603b3b4d616b15613fc3
Adam Liu <adamliu5085@gmail.com>
2024-02-26 22:44:33 -0700

    Implemented cache and domain blocking

--- HTTPproxy.py
+++ HTTPproxy.py
@@ -17,49 +17,62 @@ def ctrl_c_pressed(signal, frame):
 
 # TODO: Put function definitions here
 
-def proxy(skt):
-    # Begin to parse the accepted request
-    with skt as skt:
+def cache(cache_hostname, cache_path, cache_port, server_skt, server_connection, og_request):
 
-        # Parse receive request to a string
-        parsed_request = b""
-        while True:
-            if parsed_request.endswith(b"\r\n\r\n"):
-                break
-            temp = skt.recv(2048)
-            parsed_request += temp
+    cache_identity = cache_hostname + cache_port + cache_path
 
-        # Read the data from the client and check for properly formatted HTTP request.
-        request_pattern = re.compile(r"^([A-Z]+) ([^ ]+)\s+(HTTP/\d+\.\d+)\s+(([\w-]+: .+(?:\n[\t ]+.+)*\s+)*)$")
-        components = request_pattern.match(parsed_request.decode("utf-8"))
+    # The proxy checks if Obj is in the proxy’s cache.
+    lock.acquire()
+    try:
+        if proxy_cache.__contains__(cache_identity):
+            obj = proxy_cache.get(cache_identity)
 
-        # Send the request to client if valid
-        if components:
+            # Find the last modified time
+            last_modified_pattern = r'If-Modified-Since:\s*(.*)'
+            last_modified = re.search(last_modified_pattern, obj)
+            timestamp = ""
+            if last_modified:
+                timestamp = last_modified.group(1)
+            conditional_get = "GET " + cache_path + " HTTP/1.0" + "\r\n" + "Host: " + cache_hostname + "\r\n" + "If-Modified-Since: " + timestamp
 
-            # Call the response builder function and send it
-            request_bytes, request_port, request_host = build_response(components)
-            if request_port is None or request_host is None:
-                skt.send(request_bytes.encode('utf-8'))
+            # If so, the proxy verifies that is cached copy of Obj is up-to-date by issuing a “conditional GET” to the origin server. The proxy receives the response from the origin server.
+            server_skt.conect(server_connection)
+            server_skt.sendall(conditional_get.encode("utf-8"))
+            conditional_response = b""
+            while True:
+                t = server_skt.recv(2048)
+                if not t:
+                    break
+                conditional_response += t
 
-            # Create a new socket to send the response on the specified port
-            else:
+            # If the server’s response indicates that Obj has not been modified since it was cached by the proxy, then the proxy already has an up-to-date copy of Obj.
+            was_modified_pattern = r'304 Not Modified'
+            if re.search(was_modified_pattern, conditional_response.decode("utf-8")):
+                return obj.encode("utf-8")
 
-                # Open a new socket, send the server the parsed request, get the response, and send to client
-                with socket(AF_INET, SOCK_STREAM) as send_socket:
-                    send_socket.connect((request_host, request_port))
-                    send_socket.sendall(request_bytes.encode('utf-8'))
-                    server_response = b""
-                    while True:
-                        temp_response = send_socket.recv(2048)
-                        if not temp_response:
-                            break
-                        server_response += temp_response
-                    skt.sendall(server_response)
+            # Otherwise, the server’s response will contain an updated version of Obj.
+            return conditional_response
 
-        # Otherwise the request is bad
+        # If not, the proxy requests Obj from the origin server using a GET request.
         else:
-            bad_request = "HTTP/1.0 400 Bad Request\r\n\r\n"
-            skt.send(bad_request.encode('utf-8'))
+            server_skt.sendall(og_request)
+            conditional_response = b""
+            while True:
+                t = server_skt.recv(2048)
+                if not t:
+                    break
+                conditional_response += t
+
+            # The proxy updates its cache with the up-to-date version of Obj and the time at which Obj was last modified.
+            success_pattern = r'200 OK'
+            if re.search(success_pattern, conditional_response.decode("utf-8")):
+                proxy_cache[cache_identity] = conditional_response.decode("utf-8")
+
+            # The proxy responds to the client with the up-to-date version of Obj.
+            return conditional_response
+    finally:
+        lock.release()
+
 
 def cache_blocklist_control(control_request):
 
@@ -67,42 +80,46 @@ def cache_blocklist_control(control_request):
     control_type = control_request.group(1)
     control_action = control_request.group(2)
 
-    # Cache control
-    if control_type == "cache":
-        if control_action == "enable":
-            global cache_enabled
-            cache_enabled = True
-        elif control_action == "disable":
-            global cache_enabled
-            cache_enabled = False
-        elif control_action == "flush":
-            global proxy_cache
-            proxy_cache.clear()
+    lock.acquire()
+    try:
+        # Cache control
+        if control_type == "cache":
+            if control_action == "enable":
+                global cache_enabled
+                cache_enabled = True
+            elif control_action == "disable":
+                global cache_enabled
+                cache_enabled = False
+            elif control_action == "flush":
+                global proxy_cache
+                proxy_cache.clear()
 
-    # Blocklist control
-    if control_type == "blocklist":
-        if control_action == "enable":
-            global blocklist_enabled
-            blocklist_enabled = True
-        elif control_action == "disable":
-            global blocklist_enabled
-            blocklist_enabled = False
-        elif control_action == "add":
-            blocked_site = control_request.group(3)
-            global blocked_sites
-            blocked_sites.add(blocked_site)
-        elif control_action == "remove":
-            global blocked_sites
-            blocked_site = control_request.group(3)
-            global blocked_sites
-            blocked_sites.remove(blocked_site)
-        elif control_action == "flush":
-            global blocked_sites
-            blocked_sites.clear()
-    return "200 OK"
+        # Blocklist control
+        if control_type == "blocklist":
+            if control_action == "enable":
+                global blocklist_enabled
+                blocklist_enabled = True
+            elif control_action == "disable":
+                global blocklist_enabled
+                blocklist_enabled = False
+            elif control_action == "add":
+                blocked_site = control_request.group(3)
+                global blocked_sites
+                blocked_sites.add(blocked_site)
+            elif control_action == "remove":
+                blocked_site = control_request.group(3)
+                global blocked_sites
+                blocked_sites.remove(blocked_site)
+            elif control_action == "flush":
+                global blocked_sites
+                blocked_sites.clear()
+        return "HTTP/1.0 200 OK\r\n\r\n"
+    finally:
+        lock.release()
 
-# Build the proxy message
-# Separate the components from the regex groups
+
+
+# Build the proxy message and separate the components
 def build_response(request_components):
     # If the request is not GET, it is not implemented
     method = request_components.group(1)
@@ -121,19 +138,33 @@ def build_response(request_components):
         return "HTTP/1.0 400 Bad Request\r\n\r\n", None, None
     parsed_url = urlparse(url)
 
-    # TODO: CHECK DOMAIN BLOCKED HOSTS
+    # Check if the host is blocked or not
     host = parsed_url.hostname
+    lock.acquire()
+    try:
+        if blocklist_enabled:
+            for item in blocked_sites:
+                if item in host:
+                    return "HTTP/1.0 403 Forbidden\r\n\r\n", None, None
+    finally:
+        lock.release()
 
-    # TODO: PARSE THE PROXY CACHE CONTROL AND BLOCKLIST CONTROL
     # Get the path and ensure it is valid
     path = parsed_url.path
     if path == "":
         return "HTTP/1.0 400 Bad Request\r\n\r\n", None, None
+
+    # Parse the control patterns to change the state
     control_pattern = re.compile(r"^/proxy/(cache|blocklist)/(enable|disable|flush|add|remove)(?:/(.+))?$")
     control_components = control_pattern.match(path)
     if control_components:
-        control_response = cache_blocklist_control(control_components)
-        return control_response
+        lock.acquire()
+        try:
+            control_response = cache_blocklist_control(control_components)
+            return control_response, None, None
+        finally:
+            lock.release()
+
     # Default the port to 80 otherwise parse out the intended port
     port = 80
     if ":" in parsed_url.netloc:
@@ -154,7 +185,62 @@ def build_response(request_components):
         response = method + " " + path + " " + http_version + "\r\n" + \
                    "Host: " + host + "\r\n" + \
                    "Connection: close" + "\r\n\r\n"
-    return response, port, host
+    return response, port, host, path
+
+
+def proxy(skt):
+    # Begin to parse the accepted request
+    with skt as skt:
+
+        # Parse receive request to a string
+        parsed_request = b""
+        while True:
+            if parsed_request.endswith(b"\r\n\r\n"):
+                break
+            temp = skt.recv(2048)
+            parsed_request += temp
+
+        # Read the data from the client and check for properly formatted HTTP request.
+        request_pattern = re.compile(r"^([A-Z]+) ([^ ]+)\s+(HTTP/\d+\.\d+)\s+(([\w-]+: .+(?:\n[\t ]+.+)*\s+)*)$")
+        components = request_pattern.match(parsed_request.decode("utf-8"))
+
+        # Send the request to client if valid
+        if components:
+
+            # Call the response builder function and send it
+            request_bytes, request_port, request_host, request_path = build_response(components)
+            if request_port is None or request_host is None:
+                skt.send(request_bytes.encode('utf-8'))
+
+            # Create a new socket to send the response on the specified port
+            else:
+
+                # Open a new socket, send the server the parsed request, get the response, and send to client
+                with socket(AF_INET, SOCK_STREAM) as send_socket:
+
+                    lock.acquire()
+                    try:
+                        # Consult cache
+                        if cache_enabled:
+                            server_response = cache(request_host, request_path, request_port, send_socket, (request_host, request_port), request_bytes)
+                            skt.sendall(server_response)
+                        else:
+                            send_socket.connect((request_host, request_port))
+                            send_socket.sendall(request_bytes.encode('utf-8'))
+                            server_response = b""
+                            while True:
+                                temp_response = send_socket.recv(2048)
+                                if not temp_response:
+                                    break
+                                server_response += temp_response
+                            skt.sendall(server_response)
+                    finally:
+                        lock.release()
+        # Otherwise the request is bad
+        else:
+            bad_request = "HTTP/1.0 400 Bad Request\r\n\r\n"
+            skt.send(bad_request.encode('utf-8'))
+
 
 
 # Start of program execution
@@ -175,16 +261,13 @@ if port is None:
 # Set up signal handling (ctrl-c)
 signal.signal(signal.SIGINT, ctrl_c_pressed)
 
-# TODO: CACHE
-# TODO: CACHE TIME TO LIVE
-# TODO: DOMAIN BLOCKLIST
+# Create a lock
+lock = threading.Lock()
 
-# TODO: ENSURE WHEN TO CHANGE THIS
 # Set the cache to disabled initially
 cache_enabled = False
-proxy_cache = set()
+proxy_cache = {}
 
-# TODO: ENSURE WHEN TO CHANGE THIS
 # Set the blocklist to disabled initially, and create an empty set
 blocklist_enabled = False
 blocked_sites = set()
--- simple-test.py
+++ simple-test.py
@@ -2,6 +2,6 @@ from socket import *
 
 sock = socket(AF_INET, SOCK_STREAM)
 sock.connect(('localhost', 2100))
-# sock.sendall(b'GET http://www.flux.utah.edu/cs4480/simple.html HTTP/1.0\r\n\r\n')
+sock.sendall(b'HTTP/1.1 200 OK\r\nDate: Tue, 02 Jan 2024 20:05:47 GMT\r\nServer: Apache/2.2.24 (FreeBSD) PHP/5.3.10 with Suhosin-Patch mod_ssl/2.2.24 OpenSSL/1.0.1h DAV/2\r\nLast-Modified: Sat, 22 Jan 2022 23:19:27 GMT\r\nETag: "206accd-c2-5d633f776fdc0"\r\nAccept-Ranges: bytes\r\nContent-Length: 194\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\n<html lang="en">\n<head>\n  <meta charset="utf-8">\n  <title>Simple Page</title>\n</head>\n\n<body>\n<h1>Simple Page</h1>\n\n<p>\n  Hello!  This is simple HTML page.\n</p>\n</body>\n\n</html>\n')
 
 print(sock.makefile('rb').read())
